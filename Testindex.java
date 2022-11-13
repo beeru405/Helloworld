7b14a20e9e80181ee243f594497e2bb6ff7dcc4a
beeru405 <101712802+beeru405@users.noreply.github.com>
2022-11-13 17:54:37 +0530

    Create Testindex.java

---
+++ Testindex.java
@@ -0,0 +1,12 @@
+package com.aks;
+public class Test1 {
+    public static void main(String[] ards) {
+      String results = Test.m1();
+      System.out.println(results);
+      String[] res = results.split(" ");
+      if (res[0].equals("Hello")) {
+        System.out.println("Test Passed");
+      } else {
+        System.out.println("Test Failed");
+      }
+    }
